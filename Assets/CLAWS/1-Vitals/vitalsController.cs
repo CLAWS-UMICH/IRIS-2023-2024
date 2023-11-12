0ae1c89c27efc6bdb489a79f16643b79a550eac0
exsong <exsong@umich.edu>
2023-11-12 14:40:20 -0500

    added more vitals objects

--- Assets/CLAWS/1-Vitals/vitalsController.cs
+++ Assets/CLAWS/1-Vitals/vitalsController.cs
@@ -10,7 +10,7 @@ public class vitalsController : MonoBehaviour
     private Subscription<FellowAstronautVitalsDataChangeEvent> fellowVitalsUpdateEvent;
     public GameObject parent;
     public GameObject parentFellow;
-    TextMeshPro heartRate, H2O, O2, heartRate1, H2O1, O21;
+    TextMeshPro heartRate, H2O, O2, O2timeLeft, O2rate, H2OtimeLeft, heartRate1, H2O1, O21;
 
     void Start()
     {
@@ -19,6 +19,9 @@ public class vitalsController : MonoBehaviour
         heartRate = parent.transform.Find("HeartRate").gameObject.GetComponent<TextMeshPro>();
         H2O = parent.transform.Find("H2O").gameObject.GetComponent<TextMeshPro>();
         O2 = parent.transform.Find("O2").gameObject.GetComponent<TextMeshPro>();
+        O2timeLeft = parent.transform.Find("O2TimeLeft").gameObject.GetComponent<TextMeshPro>();
+        H2OtimeLeft = parent.transform.Find("H2OTimeLeft").gameObject.GetComponent<TextMeshPro>();
+        O2rate = parent.transform.Find("O2Rate").gameObject.GetComponent<TextMeshPro>();
     }
 
     private void onVitalsUpdate(VitalsUpdatedEvent e)
@@ -26,6 +29,11 @@ public class vitalsController : MonoBehaviour
         heartRate.text = "HeartRate: " + e.vitals.heart_rate.ToString();
         H2O.text = "H2O: " + e.vitals.h2o_gas_pressure.ToString();
         O2.text = "O2: " + e.vitals.primary_oxygen.ToString();
+        O2timeLeft.text = "O2 time remaining: " + e.vitals.o2_time_left.ToString();
+        H2OtimeLeft.text = "H2O time remaining: " + e.vitals.h2o_time_left.ToString();
+        O2rate.text = "O2 Rate: " + e.vitals.o2_rate.ToString();
+
+        // Add warning screens + when rates are going to fast
     }
 
     private void onFellowVitalsUpdate(FellowAstronautVitalsDataChangeEvent e) {
