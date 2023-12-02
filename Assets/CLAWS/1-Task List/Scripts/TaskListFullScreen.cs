b978cb906a29f666c9277e98a7de3c5e9548a3a3
pnhalim <pnhalim@umich.edu>
2023-12-02 11:43:55 -0500

    [tasklist] instantiate task UI

--- Assets/CLAWS/1-Task List/Scripts/TaskListFullScreen.cs
+++ Assets/CLAWS/1-Task List/Scripts/TaskListFullScreen.cs
@@ -9,12 +9,68 @@ using TMPro;
 
 public class TaskListFullScreen : MonoBehaviour
 {
+    [SerializeField] List<GameObject> TaskList_List;
+    [SerializeField] GameObject TaskPrefab;
+    [SerializeField] ScrollHandler TaskList_ScrollHandler;
+
+    [ContextMenu("func RenderTaskList")]
+    void RenderTaskList()
+    {
+        ClearTaskList();
+
+        foreach (TaskObj taskobj in AstronautInstance.User.TasklistData.AllTasks)
+        {
+            AddTask(taskobj);
+        }
+    }
+
+    /* Add a task to the tasklist display
+     * Note: This only adds to the tasklist display, not the actual backend */
+    void AddTask(TaskObj taskobj_f)
+    {
+        GameObject g = Instantiate(TaskPrefab, TaskList_ScrollHandler.transform);
+        // TODO update g with the correct stuff
+        TaskList_ScrollHandler.Fix();
+        TaskList_List.Add(g);
+    }
+
+    [ContextMenu("func AddTask")]
+    void Debug_AddTask()
+    {
+        TaskObj t = new();
+        t.title = "This is a debug task";
+        t.isEmergency = false;
+        AddTask(t);
+    }
+
+    /* Note: This only clears the tasklist display, not the actual list in the backend */
+    void ClearTaskList()
+    {
+        foreach (GameObject g in TaskList_List)
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                DestroyImmediate(g);
+            }
+            else
+#endif
+            {
+                Destroy(g);
+            }
+        }
+    }
+
+
+    // Event Subscriptions
+
     private Subscription<TasksDeletedEvent> tasksDeletedEvent;
     private Subscription<TasksEditedEvent> tasksEditedEvent;
     private Subscription<TasksAddedEvent> tasksAddedEvent;
 
     void Start()
     {
+        TaskList_List = new();
         tasksDeletedEvent = EventBus.Subscribe<TasksDeletedEvent>(OnTaskDeleted);
         tasksEditedEvent = EventBus.Subscribe<TasksEditedEvent>(OnTaskEdited);
         tasksAddedEvent = EventBus.Subscribe<TasksAddedEvent>(OnTaskAdded);
@@ -38,18 +94,18 @@ public class TaskListFullScreen : MonoBehaviour
     }
 
     void OnTaskDeleted(TasksDeletedEvent e)
-    { 
-
+    {
+        RenderTaskList();
     }
 
     void OnTaskEdited(TasksEditedEvent e)
     {
-        
+        RenderTaskList();
     }
 
     void OnTaskAdded(TasksAddedEvent e)
     {
-        
+        RenderTaskList();
     }
 
 }
