0f558850450673e555699f7f8a3e0d465bbea700
exsong <exsong@umich.edu>
2023-12-23 22:37:59 -0700

    progress bars working w/o indicators

--- Assets/CLAWS/1-Vitals/ProgressAnimation.cs
+++ Assets/CLAWS/1-Vitals/ProgressAnimation.cs
@@ -2,47 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ProgressAnimation : MonoBehaviour
 {
-    public Image imageToFill;
-    public GameObject vitalsMask;
     private Subscription<VitalsUpdatedEvent> vitalsUpdateEvent;
     private Subscription<FellowAstronautVitalsDataChangeEvent> fellowVitalsUpdateEvent;
-    public float vitalsData;
-    Transform quadTransform;
-    public float maxDataValue;
-    public float maxRotation;
-    public float fillPercentage;
+    private float maxScale;
 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject FullProgressBar;
+    public GameObject EmptyProgressBar;
+    public TextMeshPro value;
+    public GameObject indicator;
+
+    //adjust as needed
+    public float maxVal;
+    public float val;
+    public float fill;
+
+    //for indicator
+    private Vector3 currentPosition;
+    private Vector3 objectSize;
+
+    private void Start()
     {
-        fillPercentage = 0.5f;
-        vitalsMask.SetActive(false);
-        maxDataValue = 100f; // Maximum data value (0-100)
-        maxRotation = 360f;
-        quadTransform = vitalsMask.transform;
-        vitalsData = 0;
         vitalsUpdateEvent = EventBus.Subscribe<VitalsUpdatedEvent>(onVitalsUpdate);
-        fellowVitalsUpdateEvent = EventBus.Subscribe<FellowAstronautVitalsDataChangeEvent>(onFellowVitalsUpdate);
-    }
-    public void onVitalsUpdate(VitalsUpdatedEvent e)
-    {
-        // can't test if this is working since most recent pull messed things up :(
-        // missing prefabs and other things????
-        // float clampedProgress = Mathf.Clamp(vitalsData, 0f, 250f);
-        // Does this really work though?
+        //fellowVitalsUpdateEvent = EventBus.Subscribe<FellowAstronautVitalsDataChangeEvent>(onFellowVitalsUpdate);
+        FullProgressBar.SetActive(false);
+        val = 0f;
+        maxVal = 100f;
+        maxScale = FullProgressBar.transform.localScale.x;
 
-        vitalsData = e.vitals.heart_rate;
-        vitalsMask.SetActive(true);
-        float fillPercentage = Mathf.Clamp01(e.vitals.heart_rate / maxDataValue);
-        float fillAngle = fillPercentage * maxRotation;
-        vitalsMask.transform.localRotation = Quaternion.Euler(0f, 0f, -fillAngle);
+        ////for indicator
+        //currentPosition = FullProgressBar.transform.position;
+        //objectSize = FullProgressBar.transform.localScale;
+    }
 
-    }
-    public void onFellowVitalsUpdate(FellowAstronautVitalsDataChangeEvent e)
+    //private void Update()
+    //{
+    //    //testing with heart rate
+    //    value.text = val.ToString();
+
+    //    // Limit fill within the range of 0 to current scale
+    //    fill = Mathf.Clamp(val/maxVal * maxScale , 0f, maxScale);
+    //    //Debug.Log(fill);
+
+    //    // Adjust the scale of the fill bar based on the fill
+    //    Vector3 newScale = FullProgressBar.transform.localScale;
+    //    newScale.x = fill;
+    //    FullProgressBar.transform.localScale = newScale;
+    //    FullProgressBar.SetActive(true);
+    //}
+
+    // can change to Update if needed
+    // doesn't show in Game Mode
+    private void onVitalsUpdate(VitalsUpdatedEvent e)
     {
-        // Is this needed?
+        //testing with heart rate
+        val = e.vitals.heart_rate;
+        value.text = val.ToString();
+
+        // limit fill
+        fill = Mathf.Clamp(val / maxVal * maxScale, 0f, maxScale);
+
+        // adjust the scale of the fill bar based on the fill
+        Vector3 newScale = FullProgressBar.transform.localScale;
+        newScale.x = fill;
+
+        //// move indicator fix this if needed
+        //float rightEdge = currentPosition.x + (objectSize.x * 0.5f);
+        //Debug.Log(rightEdge);
+        //indicator.transform.position = new Vector3(rightEdge, currentPosition.y, currentPosition.z);
+
+        // update everything
+        FullProgressBar.transform.localScale = newScale;
+        FullProgressBar.SetActive(true);
     }
+
 }
