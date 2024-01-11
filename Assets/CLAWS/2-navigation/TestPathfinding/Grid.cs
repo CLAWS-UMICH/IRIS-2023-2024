4c62322fe53034ea058f8b346a3559d4169141b0
btschneid <btschneid3@gmail.com>
2024-01-10 20:52:20 -0500

    Added pathfdining code to navigation

---
+++ Assets/CLAWS/2-navigation/TestPathfinding/Grid.cs
@@ -0,0 +1,274 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+
+    public Transform StartPosition;//This is where the program will start the pathfinding from.
+    public LayerMask WallMask;//This is the mask that the program will look for when trying to find obstructions to the path.
+    public Vector2 vGridWorldSize;//A vector2 to store the width and height of the graph in world units.
+    public float fNodeRadius;//This stores how big each square on the graph will be
+    public float fDistanceBetweenNodes;//The distance that the squares will spawn from eachother.
+    public TerrainType[] walkableRegions; // Areas to walk accompanied with their penalty
+    LayerMask walkableMask;
+    Dictionary<int, int> walkableRegionsDict = new Dictionary<int, int>();
+
+    Node[,] NodeArray;//The array of nodes that the A Star algorithm uses.
+    public List<Node> FinalPath;//The completed path that the red line will be drawn along
+
+
+    float fNodeDiameter;//Twice the amount of the radius (Set in the start function)
+    int iGridSizeX, iGridSizeY;//Size of the Grid in Array units.
+
+    public int MaxSize
+    {
+        get
+        {
+            return iGridSizeX * iGridSizeY;
+        }
+    }
+
+    public int blurSize = 3;
+
+
+
+    private void Start()//Ran once the program starts
+    {
+        fNodeDiameter = fNodeRadius * 2;//Double the radius to get diameter
+        iGridSizeX = Mathf.RoundToInt(vGridWorldSize.x / fNodeDiameter);//Divide the grids world co-ordinates by the diameter to get the size of the graph in array units.
+        iGridSizeY = Mathf.RoundToInt(vGridWorldSize.y / fNodeDiameter);//Divide the grids world co-ordinates by the diameter to get the size of the graph in array units.
+        foreach(TerrainType region in walkableRegions)
+        {
+            walkableMask.value = walkableMask | region.terrainMask.value;
+            walkableRegionsDict.Add((int)Mathf.Log(region.terrainMask.value, 2), region.terrainPenalty);
+        }
+        CreateGrid();//Draw the grid
+    }
+
+    void CreateGrid()
+    {
+        NodeArray = new Node[iGridSizeX, iGridSizeY];//Declare the array of nodes.
+        Vector3 bottomLeft = transform.position - Vector3.right * vGridWorldSize.x / 2 - Vector3.forward * vGridWorldSize.y / 2;//Get the real world position of the bottom left of the grid.
+        for (int x = 0; x < iGridSizeX; x++)//Loop through the array of nodes.
+        {
+            for (int y = 0; y < iGridSizeY; y++)//Loop through the array of nodes
+            {
+                Vector3 worldPoint = bottomLeft + Vector3.right * (x * fNodeDiameter + fNodeRadius) + Vector3.forward * (y * fNodeDiameter + fNodeRadius);//Get the world co ordinates of the bottom left of the graph
+                bool Wall = true;//Make the node a wall
+
+                //If the node is not being obstructed
+                //Quick collision check against the current node and anything in the world at its position. If it is colliding with an object with a WallMask,
+                //The if statement will return false.
+                if (Physics.CheckSphere(worldPoint, fNodeRadius, WallMask))
+                {
+                    Wall = false;//Object is not a wall
+                }
+
+                int movementPenalty = 0;
+
+                if (Wall)
+                {
+                    Ray ray = new Ray(worldPoint + Vector3.up * 50, Vector3.down);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit, 100, walkableMask))
+                    {
+                        walkableRegionsDict.TryGetValue(hit.collider.gameObject.layer, out movementPenalty);
+                    }
+                }
+                NodeArray[x, y] = new Node(Wall, worldPoint, x, y, movementPenalty);//Create a new node in the array.
+            }
+        }
+        BlurPenaltyMap(blurSize);
+    }
+
+    void BlurPenaltyMap(int blurSize)
+    {
+        int kernelSize = blurSize * 2 + 1;
+        int kernelExtents = (kernelSize - 1) / 2;
+
+        int[,] penaltiesHorizantalPass = new int[iGridSizeX, iGridSizeY];
+        int[,] penaltiesVerticalPass = new int[iGridSizeX, iGridSizeY];
+
+        for (int y = 0; y < iGridSizeY; y++)
+        {
+            for (int x = -kernelExtents; x <= kernelExtents; x++)
+            {
+                int sampleX = Mathf.Clamp(x, 0, kernelExtents);
+                penaltiesHorizantalPass[0, y] += NodeArray[sampleX, y].movementPenalty;
+            }
+
+            for (int x = 1; x < iGridSizeX; x++)
+            {
+                int removeIndex = Mathf.Clamp(x - kernelExtents - 1, 0, iGridSizeX);
+                int addIndex = Mathf.Clamp(x + kernelExtents, 0, iGridSizeX - 1);
+
+                penaltiesHorizantalPass[x, y] = penaltiesHorizantalPass[x - 1, y] - NodeArray[removeIndex, y].movementPenalty + NodeArray[addIndex, y].movementPenalty;
+            }
+        }
+
+        for (int x = 0; x < iGridSizeX; x++)
+        {
+            for (int y = -kernelExtents; y <= kernelExtents; y++)
+            {
+                int sampleY = Mathf.Clamp(y, 0, kernelExtents);
+                penaltiesVerticalPass[x, 0] += NodeArray[x, sampleY].movementPenalty;
+            }
+            int blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x, 0] / (kernelSize * kernelSize));
+            NodeArray[x, 0].movementPenalty = blurredPenalty;
+
+            for (int y = 1; y < iGridSizeY; y++)
+            {
+                int removeIndex = Mathf.Clamp(y - kernelExtents - 1, 0, iGridSizeY);
+                int addIndex = Mathf.Clamp(y + kernelExtents, 0, iGridSizeY - 1);
+
+                penaltiesVerticalPass[x, y] = penaltiesVerticalPass[x, y - 1] - NodeArray[x, removeIndex].movementPenalty + NodeArray[x, addIndex].movementPenalty;
+                blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x, y] / (kernelSize * kernelSize));
+                NodeArray[x, y].movementPenalty = blurredPenalty;
+
+            }
+        }
+    }
+
+    //Function that gets the neighboring nodes of the given node.
+    public List<Node> GetNeighboringNodes(Node a_NeighborNode)
+    {
+        List<Node> NeighborList = new List<Node>();//Make a new list of all available neighbors.
+        int icheckX;//Variable to check if the XPosition is within range of the node array to avoid out of range errors.
+        int icheckY;//Variable to check if the YPosition is within range of the node array to avoid out of range errors.
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                {
+                    continue;
+                }
+
+                icheckX = a_NeighborNode.iGridX + x;
+                icheckY = a_NeighborNode.iGridY + y;
+
+                if (icheckX >= 0 && icheckX < iGridSizeX)
+                {
+                    if (icheckY >= 0 && icheckY < iGridSizeY)
+                    {
+                        NeighborList.Add(NodeArray[icheckX, icheckY]);
+                    }
+                }
+            }
+        }
+
+        return NeighborList;
+
+
+
+
+
+
+
+        /*
+        //Check the right side of the current node.
+        icheckX = a_NeighborNode.iGridX + 1;
+        icheckY = a_NeighborNode.iGridY;
+        if (icheckX >= 0 && icheckX < iGridSizeX)//If the XPosition is in range of the array
+        {
+            if (icheckY >= 0 && icheckY < iGridSizeY)//If the YPosition is in range of the array
+            {
+                NeighborList.Add(NodeArray[icheckX, icheckY]);//Add the grid to the available neighbors list
+            }
+        }
+        //Check the Left side of the current node.
+        icheckX = a_NeighborNode.iGridX - 1;
+        icheckY = a_NeighborNode.iGridY;
+        if (icheckX >= 0 && icheckX < iGridSizeX)//If the XPosition is in range of the array
+        {
+            if (icheckY >= 0 && icheckY < iGridSizeY)//If the YPosition is in range of the array
+            {
+                NeighborList.Add(NodeArray[icheckX, icheckY]);//Add the grid to the available neighbors list
+            }
+        }
+        //Check the Top side of the current node.
+        icheckX = a_NeighborNode.iGridX;
+        icheckY = a_NeighborNode.iGridY + 1;
+        if (icheckX >= 0 && icheckX < iGridSizeX)//If the XPosition is in range of the array
+        {
+            if (icheckY >= 0 && icheckY < iGridSizeY)//If the YPosition is in range of the array
+            {
+                NeighborList.Add(NodeArray[icheckX, icheckY]);//Add the grid to the available neighbors list
+            }
+        }
+        //Check the Bottom side of the current node.
+        icheckX = a_NeighborNode.iGridX;
+        icheckY = a_NeighborNode.iGridY - 1;
+        if (icheckX >= 0 && icheckX < iGridSizeX)//If the XPosition is in range of the array
+        {
+            if (icheckY >= 0 && icheckY < iGridSizeY)//If the YPosition is in range of the array
+            {
+                NeighborList.Add(NodeArray[icheckX, icheckY]);//Add the grid to the available neighbors list
+            }
+        }
+
+        return NeighborList;//Return the neighbors list.
+        */
+    }
+
+    //Gets the closest node to the given world position.
+    public Node NodeFromWorldPoint(Vector3 a_vWorldPos)
+    {
+        float ixPos = ((a_vWorldPos.x + vGridWorldSize.x / 2) / vGridWorldSize.x);
+        float iyPos = ((a_vWorldPos.z + vGridWorldSize.y / 2) / vGridWorldSize.y);
+
+        ixPos = Mathf.Clamp01(ixPos);
+        iyPos = Mathf.Clamp01(iyPos);
+
+        int ix = Mathf.RoundToInt((iGridSizeX - 1) * ixPos);
+        int iy = Mathf.RoundToInt((iGridSizeY - 1) * iyPos);
+
+        return NodeArray[ix, iy];
+    }
+
+
+    //Function that draws the wireframe
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.DrawWireCube(transform.position, new Vector3(vGridWorldSize.x, 1, vGridWorldSize.y));//Draw a wire cube with the given dimensions from the Unity inspector
+
+        if (NodeArray != null)//If the grid is not empty
+        {
+            foreach (Node n in NodeArray)//Loop through every node in the grid
+            {
+                if (n.bIsWall)//If the current node is a wall node
+                {
+                    Gizmos.color = Color.white;//Set the color of the node
+                }
+                else
+                {
+                    Gizmos.color = Color.yellow;//Set the color of the node
+                }
+
+
+                if (FinalPath != null)//If the final path is not empty
+                {
+                    if (FinalPath.Contains(n))//If the current node is in the final path
+                    {
+                        Gizmos.color = Color.red;//Set the color of that node
+                    }
+
+                }
+
+
+                Gizmos.DrawCube(n.vPosition, Vector3.one * (fNodeDiameter - fDistanceBetweenNodes));//Draw the node at the position of the node.
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class TerrainType
+    {
+        public LayerMask terrainMask;
+        public int terrainPenalty;
+
+    }
+}
