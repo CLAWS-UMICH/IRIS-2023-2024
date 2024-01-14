1c51e9f05744d9786cdd74d9e2afd86aa0490ada
pnhalim <pnhalim@umich.edu>
2024-01-14 18:18:18 -0500

    [tasklist] Polish detailed view UI

--- Assets/CLAWS/1-Task List/Scripts/DetailedTask.cs
+++ Assets/CLAWS/1-Task List/Scripts/DetailedTask.cs
@@ -10,16 +10,33 @@ public class DetailedTask : MonoBehaviour
     [SerializeField] TextMeshPro TaskData;
     [SerializeField] TextMeshPro TaskMetaData;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
+    [SerializeField] Transform Backplate;
 
     public void InitDetailedView(string name, string data, string metadata)
     {
         TaskName.text = name;
         TaskData.text = data;
         TaskMetaData.text = metadata;
+
+        SetBackplateSize();
+    }
+
+    [ContextMenu("func SetBackplateSize")]
+    public void SetBackplateSize()
+    {
+        // set backplate size
+        float init_y = Backplate.localPosition.y;
+        float init_height = Backplate.localScale.y;
+
+        float height = 0.065f + (0.0107f * TaskData.textInfo.lineCount);
+        float y = init_y - (height - init_height) / 2f;
+
+        Backplate.localScale = new(Backplate.localScale.x, height, Backplate.localScale.z);
+        Backplate.localPosition = new(Backplate.localPosition.x, y, Backplate.localPosition.z);
+    }
+
+    public void OnCloseButtonPressed()
+    {
+        Destroy(gameObject);
     }
 }
