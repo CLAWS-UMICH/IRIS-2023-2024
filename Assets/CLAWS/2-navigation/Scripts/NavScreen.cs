758b4157ae49f179fd180805381a511cc05d2607
selinal <selnal@umich.edu>
2024-01-27 12:07:27 -0500

    s

---
+++ Assets/CLAWS/2-navigation/Scripts/NavScreen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavScreenHandler : MonoBehaviour
+{
+    GameObject parent;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
