63649c49459a822a842e7f75797c68fbfa2dc378
Adhav Rajesh <adhavrajesh04@gmail.com>
2024-03-27 21:31:49 -0400

    Cooked

--- Assets/CLAWS/Scripts/CameraMovement.cs
+++ Assets/CLAWS/Scripts/CameraMovement.cs
@@ -1,41 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    Coroutine lunaCoroutine;
-    [SerializeField] private float timeDelay = 1f;
     [SerializeField] GameObject playerCam;
     public float offset = 0f;
+    float zeroValue = 0f;
 
+    private UdpClient serverSocket;
     void Start()
     {
-        EnterLunaMode();
-    }
-
-    IEnumerator LunaMove()
-    {
-        while (true)
+        try
         {
-            yield return new WaitForSeconds(timeDelay);
-            gameObject.transform.rotation = Quaternion.Euler(0, playerCam.transform.rotation.eulerAngles.y + offset, 0);
+            // Create a UDP socket
+            serverSocket = new UdpClient(8888);
+            Debug.Log("UDP socket created, waiting for messages...");
+            // Start listening in a separate thread
+            StartListening();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error creating UDP socket: " + e.Message);
         }
     }
-    [ContextMenu("Luna Follow")]
-    public void EnterLunaMode()
+    private void StartListening()
     {
-        lunaCoroutine = StartCoroutine(LunaMove());
+        try
+        {
+            // Begin receiving messages asynchronously
+            serverSocket.BeginReceive(ReceiveCallback, null);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error starting to listen for messages: " +e.Message);
+        }
     }
-    [ContextMenu("Luna Fix")]
-    public void StopLunaMode()
+    private void ReceiveCallback(IAsyncResult result)
     {
-        StopCoroutine(lunaCoroutine);
+        try
+        {
+            // Get the client?s message and endpoint
+            IPEndPoint clientEndpoint = new IPEndPoint(IPAddress.Any, 0);
+            byte[] receivedData = serverSocket.EndReceive(result, ref clientEndpoint);
+            // Convert the byte array to a string
+            string message = Encoding.ASCII.GetString(receivedData);
+            // Print the received message
+            offset = (float)Convert.ToDouble(message) + zeroValue;
+            Debug.Log("Received message: " +message);
+            // Continue listening for more messages
+            StartListening();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error receiving message: " +e.Message);
+        }
+    }
+    void OnApplicationQuit()
+    {
+        // Close the UDP socket when the application quits
+        if (serverSocket != null)
+        {
+            serverSocket.Close();
+            Debug.Log("UDP socket closed.");
+        }
+    }
+    [ContextMenu("COR Sync")]
+    public void CorSync()
+    {
+        zeroValue = offset * -1;
     }
 
     // Update is called once per frame
     void Update()
     {
         gameObject.transform.position = playerCam.transform.position;
+        gameObject.transform.eulerAngles = new Vector3(
+            gameObject.transform.eulerAngles.x,
+            offset,
+            gameObject.transform.eulerAngles.z
+        );
     }
 }
