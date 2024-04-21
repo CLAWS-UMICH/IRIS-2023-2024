302ede311c1d8fb68d0c59a73a0ca9101b77553e
John <57152121+JohnRogan@users.noreply.github.com>
2024-04-21 12:57:22 -0400

    Updates to Voice Message

--- Assets/CLAWS/3-Messaging/Scripts/VoiceMessage.cs
+++ Assets/CLAWS/3-Messaging/Scripts/VoiceMessage.cs
@@ -5,15 +5,26 @@ using TMPro;
 
 #if !UNITY_WEBGL
 using UnityEngine.Windows.Speech;
+using System.Linq;
 
 public class VoiceMessage : MonoBehaviour
 {
-    [SerializeField] private TMP_Text message;
+    private TextMeshPro message_TMP;
+    private DictationRecognizer dictationRecognizer;
+    private KeywordRecognizer keywordRecognizer;
+    private bool dictationActive = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        message_TMP = transform.Find("Placeholder").GetComponent<TextMeshPro>();
+        keywordRecognizer = new KeywordRecognizer(new string[] { "Vega", "Vega Stop" });
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
 
+        dictationRecognizer = new DictationRecognizer();
+        dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+        dictationRecognizer.Start();
     }
 
     // Update is called once per frame
@@ -28,9 +39,49 @@ public class VoiceMessage : MonoBehaviour
         dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
     }
 
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        string keyword = args.text;
+        if (keyword == "Vega")
+        {
+            if (!dictationActive)
+            {
+                dictationActive = true;
+                dictationRecognizer.Start();
+                Debug.Log("Dictation started...");
+            }
+        }
+        else if (keyword == "Vega Stop")
+        {
+            if (dictationActive)
+            {
+                dictationActive = false;
+                dictationRecognizer.Stop();
+                Debug.Log("Dictation stopped...");
+
+            }
+        }
+    }
+
     private void DictationRecognizer_DictationResult(string text, ConfidenceLevel confidence)
     {
-        message.SetText(message + " " + text);
+        Debug.Log("Dictation result: " + text);
+        message_TMP.text = text;
+    }
+
+    private void OnDestroy()
+    {
+        if (keywordRecognizer != null)
+        {
+            keywordRecognizer.Stop();
+            keywordRecognizer.Dispose();
+        }
+
+        if (dictationRecognizer != null)
+        {
+            dictationRecognizer.Stop();
+            dictationRecognizer.Dispose();
+        }
     }
 }
 
