c3e11efeea1463c4460358b87baac50daa4d66b4
pnhalim <pnhalim@umich.edu>
2024-05-07 16:25:53 -0400

    [uia] remove incorrect import

--- Assets/UIAPanel.cs
+++ Assets/UIAPanel.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting.Dependencies.Sqlite;
 
 public class UIAPanel : MonoBehaviour
 {
