31ade2eb69fd97577e2674734a015cfb7c46e6f1
Brian Schneider <btschneid3@gmail.com>
2024-05-07 11:28:32 -0400

    Completed the start of VEGA backend for voice commands

--- Assets/CLAWS/3-VEGA/VEGAManager.cs
+++ Assets/CLAWS/3-VEGA/VEGAManager.cs
@@ -1,45 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using System;
 
 public class VEGAManager : MonoBehaviour
 {
-    Subscription<SpeechToText> vegaCommandEvent;
-    GameObject screen;
-    TextMeshPro text;
-    // Start is called before the first frame update
+    Subscription<VEGACommand> vegaCommandEvent;
+
+    private Dictionary<Screens, List<string>> screenAssociatedCommands;
+    private Dictionary<Modes, List<string>> modeAssociatedCommands;
+    private Dictionary<string, (Action<List<string>>, int)> functionDictionary;
+
     void Start()
     {
-        vegaCommandEvent = EventBus.Subscribe<SpeechToText>(onNewVEGACommand);
-        screen = transform.Find("Screen").gameObject;
-        text = transform.Find("Screen").Find("text").transform.GetComponent<TextMeshPro>();
-        text.text = "";
-        screen.SetActive(false);
+        vegaCommandEvent = EventBus.Subscribe<VEGACommand>(onVEGACommand);
+
+        // Initialize screen-associated commands
+        screenAssociatedCommands = new Dictionary<Screens, List<string>>
+        {
+            { Screens.Navigation, new List<string> { "apple", "banana", "orange" } },
+            { Screens.NavConfirmation, new List<string> { "car", "bike", "bus" } },
+        };
+
+        // Initialize mode-associated commands
+        modeAssociatedCommands = new Dictionary<Modes, List<string>>
+        {
+            { Modes.Navigation, new List<string> { "apple", "banana" } },
+            { Modes.Normal, new List<string> { "car", "bus" } },
+        };
+
+        // Initialize function dictionary with function name and parameter count
+        functionDictionary = new Dictionary<string, (Action<List<string>>, int)>
+        {
+            { "apple", (FunctionForApple, 0) },
+            { "banana", (FunctionForBanana, 0) },
+            { "orange", (FunctionForOrange, 3) },
+            { "car", (FunctionForCar, 0) },
+            { "bike", (FunctionForBike, 0) },
+            { "bus", (FunctionForBus, 1) },
+        };
     }
 
-    private void onNewVEGACommand(SpeechToText c)
+    private void onVEGACommand(VEGACommand c)
     {
-        text.text = c.text;
-        StartCoroutine(CloseScreenWithDelay(2));
+        Screens currentScreen = StateMachine.CurrScreen;
+        Modes currentMode = StateMachine.CurrMode;
+
+        // Check if the command matches any associated command for the current screen or mode
+        if (screenAssociatedCommands.ContainsKey(currentScreen) && screenAssociatedCommands[currentScreen].Contains(c.command_name))
+        {
+            ExecuteCommand(c.command_name, c.arguments);
+        }
+        else if (modeAssociatedCommands.ContainsKey(currentMode) && modeAssociatedCommands[currentMode].Contains(c.command_name))
+        {
+            ExecuteCommand(c.command_name, c.arguments);
+        }
+        else
+        {
+            Debug.LogError("Command name does not match any associated command for the current screen or mode");
+        }
     }
 
-    private IEnumerator CloseScreenWithDelay(float delay)
+    private void ExecuteCommand(string commandName, List<string> arguments)
     {
-        yield return new WaitForSeconds(delay);
-        screen.SetActive(false);
+        if (functionDictionary.ContainsKey(commandName))
+        {
+            var (action, parameterCount) = functionDictionary[commandName];
+            if (arguments.Count == parameterCount)
+            {
+                action(arguments);
+            }
+            else
+            {
+                Debug.LogError($"Command '{commandName}' requires {parameterCount} parameters");
+            }
+        }
+        else
+        {
+            Debug.LogError($"Command '{commandName}' not found in function dictionary");
+        }
     }
 
-    /*public void StartVEGA()
+    // Example functions without parameters
+    private void FunctionForApple(List<string> parameters)
     {
-        SpeechRecognitionManager.SwitchToDictation(0);
-        text.text = "";
-        screen.SetActive(true);
+        Debug.Log("Function for apple called.");
     }
 
-    public void EndVEGA()
+    private void FunctionForBanana(List<string> parameters)
     {
-        SpeechRecognitionManager.SwitchToPhraseRecognition();
-        text.text = "";
-    }*/
+        Debug.Log("Function for banana called.");
+    }
+
+    private void FunctionForOrange(List<string> parameters)
+    {
+        Debug.Log("Function for orange called.");
+    }
+
+    private void FunctionForCar(List<string> parameters)
+    {
+        Debug.Log("Function for car called.");
+    }
+
+    private void FunctionForBike(List<string> parameters)
+    {
+        Debug.Log("Function for bike called.");
+    }
+
+    private void FunctionForBus(List<string> parameters)
+    {
+        Debug.Log("Function for bus called.");
+    }
 }
---
+++ Assets/CLAWS/3-VEGA/VEGAScreenHandler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VEGAScreenHandler : MonoBehaviour
+{
+    Subscription<SpeechToText> vegaCommandEvent;
+    GameObject screen;
+    TextMeshPro text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vegaCommandEvent = EventBus.Subscribe<SpeechToText>(onNewVEGACommand);
+        screen = transform.Find("Screen").gameObject;
+        text = transform.Find("Screen").Find("text").transform.GetComponent<TextMeshPro>();
+        text.text = "";
+        screen.SetActive(false);
+    }
+
+    private void onNewVEGACommand(SpeechToText c)
+    {
+        screen.SetActive(true);
+        text.text = c.text;
+        StartCoroutine(CloseScreenWithDelay(2));
+    }
+
+    private IEnumerator CloseScreenWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        screen.SetActive(false);
+    }
+
+    /*public void StartVEGA()
+    {
+        SpeechRecognitionManager.SwitchToDictation(0);
+        text.text = "";
+        screen.SetActive(true);
+    }
+
+    public void EndVEGA()
+    {
+        SpeechRecognitionManager.SwitchToPhraseRecognition();
+        text.text = "";
+    }*/
+}
